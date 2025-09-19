993f56225b22aa3cda53215e503098b888ae9e63
Ameer Hamza <Ameerhamza4207.com>
2025-09-19 11:05:38 +0500

    update title

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Welcome to National Computing Education Accreditation Council!');
+  protected readonly title = signal('Welcome to Nceac App!');
 }

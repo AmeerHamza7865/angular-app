6aec3229149659d948328adac464cd31ec35fac1
Ameer Hamza <Ameerhamza4207.com>
2025-09-19 12:02:09 +0500

    Navbar and pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Pagenotfound } from './pages/pagenotfound/pagenotfound';
+import { Contact } from './pages/contact/contact';
+import { About } from './pages/about/about';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'about', component: About },
+    { path: 'contact', component: Contact },
+    { path: '**', component: Pagenotfound }
+];
